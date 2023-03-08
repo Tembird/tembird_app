3225c6779e23fae7b580800e814614e12ce59c68
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-03-08 23:02:25 +0900

    [Design] Create Styled Files as Constant

---
+++ lib/constant/StyledFont.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'StyledPalette.dart';
+
+class StyledFont {
+  static const TextStyle LARGE_TITLE = TextStyle(fontWeight: FontWeight.w400, fontSize: 34, height: 41 / 34, color: StyledPalette.BLACK);
+  static const TextStyle TITLE_1 = TextStyle(fontWeight: FontWeight.w400, fontSize: 28, height: 34 / 28, color: StyledPalette.BLACK);
+  static const TextStyle TITLE_2 = TextStyle(fontWeight: FontWeight.w400, fontSize: 22, height: 28 / 22, color: StyledPalette.BLACK);
+  static const TextStyle TITLE_3 = TextStyle(fontWeight: FontWeight.w400, fontSize: 20, height: 25 / 20, color: StyledPalette.BLACK);
+  static const TextStyle HEADLINE = TextStyle(fontWeight: FontWeight.w700, fontSize: 17, height: 22 / 17, color: StyledPalette.BLACK);
+  static const TextStyle HEADLINE_INFO = TextStyle(fontWeight: FontWeight.w700, fontSize: 17, height: 22 / 17, color: StyledPalette.STATUS_INFO);
+  static const TextStyle HEADLINE_WHITE = TextStyle(fontWeight: FontWeight.w700, fontSize: 17, height: 22 / 17, color: StyledPalette.WHITE);
+  static const TextStyle BODY = TextStyle(fontWeight: FontWeight.w400, fontSize: 17, height: 22 / 17, color: StyledPalette.BLACK);
+  static const TextStyle CALLOUT = TextStyle(fontWeight: FontWeight.w400, fontSize: 16, height: 21 / 16, color: StyledPalette.BLACK);
+  static const TextStyle CALLOUT_700 = TextStyle(fontWeight: FontWeight.w700, fontSize: 16, height: 21 / 16, color: StyledPalette.BLACK);
+  static const TextStyle SUBHEAD = TextStyle(fontWeight: FontWeight.w400, fontSize: 15, height: 20 / 15, color: StyledPalette.BLACK);
+  static const TextStyle FOOTNOTE = TextStyle(fontWeight: FontWeight.w400, fontSize: 13, height: 18 / 13, color: StyledPalette.BLACK);
+  static const TextStyle CAPTION_1 = TextStyle(fontWeight: FontWeight.w400, fontSize: 12, height: 16 / 12, color: StyledPalette.BLACK);
+  static const TextStyle CAPTION_2 = TextStyle(fontWeight: FontWeight.w400, fontSize: 11, height: 13 / 11, color: StyledPalette.BLACK);
+}
---
+++ lib/constant/StyledPalette.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class StyledPalette {
+  static const Color BLACK = Color.fromRGBO(18, 18, 18, 1);
+  static const Color GRAY = Color.fromRGBO(157, 157, 157, 1);
+  static const Color WHITE = Color.fromRGBO(255, 255, 255, 1);
+
+  static const Color PRIMARY_SKY_DARK = Color.fromRGBO(148, 191, 255, 1);
+  static const Color PRIMARY_SKY_BRIGHT = Color.fromRGBO(176, 213, 255, 1);
+  static const Color PRIMARY_SKY_BACKGROUND = Color.fromRGBO(229, 240, 254, 1);
+
+  static const Color ACCENT_YELLOW = Color.fromRGBO(246, 204, 124, 1);
+  static const Color ACCENT_RED= Color.fromRGBO(238, 127, 121, 1);
+  static const Color ACCENT_LAVENDER= Color.fromRGBO(161, 156, 247, 1);
+  static const Color ACCENT_GREEN= Color.fromRGBO(119, 221, 119, 1);
+
+  static const Color STATUS_INFO= Color.fromRGBO(37, 128, 235, 1);
+  static const Color STATUS_NEGATIVE= Color.fromRGBO(227, 72, 80, 1);
+  static const Color STATUS_NOTICE= Color.fromRGBO(230, 134, 22, 1);
+  static const Color STATUS_POSITIVE= Color.fromRGBO(46, 157, 119, 1);
+
+  static const Color TRANSPARENT = Color.fromRGBO(0, 0, 0, 0);
+}
