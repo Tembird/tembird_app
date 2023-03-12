4ab8a49af0ddc8df556db6f6b4583f2192d8523a
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-03-13 03:11:39 +0900

    [Feat] Create NativeSplashScreen

--- lib/model/Schedule.dart
+++ lib/model/Schedule.dart
@@ -22,4 +22,28 @@ class Schedule {
     required this.scheduleMember,
     required this.scheduleDone,
   });
+
+  factory Schedule.fromJson(Map<String, dynamic> json) => Schedule(
+    scheduleId: json['scheduleId'],
+    scheduleDate: DateTime.parse(json["scheduleDate"]),
+    scheduleIndexList: (json["scheduleIndexList"] as List<String>).map((e) => int.parse(e)).toList(),
+    scheduleColorHex: json["scheduleColorHex"],
+    scheduleTitle: json["scheduleTitle"],
+    scheduleDetail: json["scheduleDetail"],
+    scheduleLocation: json["scheduleLocation"],
+    scheduleMember: json["scheduleMember"] as List<String>,
+    scheduleDone: json["scheduleDone"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "scheduleId": scheduleId,
+    "scheduleDate": scheduleDate.toString(),
+    "scheduleIndexList": scheduleIndexList.toString(),
+    "scheduleColorHex": scheduleColorHex,
+    "scheduleTitle": scheduleTitle,
+    "scheduleDetail": scheduleDetail,
+    "scheduleLocation": scheduleLocation,
+    "scheduleMember": scheduleMember.toString(),
+    "scheduleDone": scheduleDone,
+  };
 }
