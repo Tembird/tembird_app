4ca69fbb0e00129c3de2015089a2e1858f3b7e42
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-03-24 01:20:37 +0900

    [Refactor] Change Keys of Schedule to Connect DB

--- lib/model/Schedule.dart
+++ lib/model/Schedule.dart
@@ -1,49 +1,68 @@
-import 'dart:math';
-
 class Schedule {
-  final int scheduleId;
-  final DateTime scheduleDate;
+  final String sid;
+  final DateTime date;
   final List<int> scheduleIndexList;
-  final String scheduleColorHex;
-  final String? scheduleTitle;
-  final String? scheduleDetail;
-  final String? scheduleLocation;
-  final List<String> scheduleMember;
-  bool scheduleDone;
+  final int startAt;
+  final int endAt;
+  final String colorHex;
+  final String? title;
+  final String? detail;
+  final String? location;
+  final List<String> memberList;
+  final DateTime createdAt;
+  DateTime editedAt;
+  bool done;
+  DateTime? doneAt;
 
   Schedule({
-    required this.scheduleId,
-    required this.scheduleDate,
+    required this.sid,
+    required this.date,
+    required this.startAt,
+    required this.endAt,
     required this.scheduleIndexList,
-    required this.scheduleColorHex,
-    this.scheduleTitle,
-    this.scheduleDetail,
-    this.scheduleLocation,
-    required this.scheduleMember,
-    required this.scheduleDone,
+    required this.colorHex,
+    this.title,
+    this.detail,
+    this.location,
+    required this.memberList,
+    required this.createdAt,
+    required this.editedAt,
+    this.done = false,
+    this.doneAt,
   });
 
-  factory Schedule.fromJson(Map<String, dynamic> json) => Schedule(
-    scheduleId: json['scheduleId'],
-    scheduleDate: DateTime.parse(json["scheduleDate"]),
-    scheduleIndexList: (json["scheduleIndexList"] as List<String>).map((e) => int.parse(e)).toList(),
-    scheduleColorHex: json["scheduleColorHex"],
-    scheduleTitle: json["scheduleTitle"],
-    scheduleDetail: json["scheduleDetail"],
-    scheduleLocation: json["scheduleLocation"],
-    scheduleMember: json["scheduleMember"] as List<String>,
-    scheduleDone: json["scheduleDone"],
-  );
+  factory Schedule.fromJson(Map<String, dynamic> json) {
+    int startAt = json["start_at"];
+    int endAt = json["end_at"];
+    return Schedule(
+      sid: json['sid'],
+      date: DateTime.parse(json["date"]),
+      scheduleIndexList: List.generate(endAt - startAt + 1, (index) => startAt + index),
+      colorHex: json["color_hex"],
+      startAt: startAt,
+      endAt: endAt,
+      title: json["title"],
+      detail: json["detail"],
+      location: json["location"],
+      memberList: (json["member_list"] as String).split(','),
+      done: json["done"] == 1,
+      doneAt: json["done_at"] == null ? null : DateTime.parse(json["done_at"]),
+      createdAt: DateTime.parse(json["created_at"]),
+      editedAt: DateTime.parse(json["edited_at"]),
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-    "scheduleId": scheduleId,
-    "scheduleDate": scheduleDate.toString(),
-    "scheduleIndexList": scheduleIndexList.toString(),
-    "scheduleColorHex": scheduleColorHex,
-    "scheduleTitle": scheduleTitle,
-    "scheduleDetail": scheduleDetail,
-    "scheduleLocation": scheduleLocation,
-    "scheduleMember": scheduleMember.toString(),
-    "scheduleDone": scheduleDone,
-  };
+  // Map<String, dynamic> toJson() => {
+  //   "sid": sid,
+  //   "date": DateFormat('yyMMdd').format(date),
+  //   "colorHex": colorHex,
+  //   "startAt": scheduleIndexList.first,
+  //   "endAt": scheduleIndexList.last,
+  //   "title": title,
+  //   "detail": detail,
+  //   "location": location,
+  //   "memberList": memberList.toString(),
+  //   "done": done,
+  //   "doneAt": doneAt,
+  // };
 }
--- lib/repository/ScheduleRepository.dart
+++ lib/repository/ScheduleRepository.dart
@@ -1,97 +1,41 @@
 import 'package:get/get.dart';
 import 'package:tembird_app/model/Schedule.dart';
 import 'package:tembird_app/repository/RootRepository.dart';
+import 'package:intl/intl.dart';
 
 class ScheduleRepository extends RootRepository {
   static ScheduleRepository to = Get.find();
 
+  ScheduleRepository() {
+    initialization();
+  }
+
   Future<List<Schedule>> readScheduleListOnDate({required DateTime dateTime}) async {
-    // TODO : Connect API to Read Schedule List on Date
-    // await Future.delayed(const Duration(seconds: 2));
-
     List<Schedule> scheduleList = [];
-
-    if (dateTime.isBefore(DateTime.now())) {
-      scheduleList = [
-        Schedule(
-          scheduleId: 1,
-          scheduleDate: DateTime(2023, 3, 10),
-          scheduleIndexList: [13, 14, 15],
-          scheduleColorHex: "859039",
-          scheduleTitle: "코딩 공부",
-          scheduleDetail: "DateTime 공부",
-          scheduleDone: false,
-          scheduleMember: [],
-        ),
-        Schedule(
-          scheduleId: 1,
-          scheduleDate: DateTime(2023, 3, 10),
-          scheduleIndexList: [19, 20, 21, 22],
-          scheduleColorHex: "859039",
-          scheduleTitle: "코딩 공부",
-          scheduleDetail: "DateTime 공부",
-          scheduleDone: false,
-          scheduleMember: [],
-        ),
-        Schedule(
-          scheduleId: 1,
-          scheduleDate: DateTime(2023, 3, 10),
-          scheduleIndexList: [25, 26],
-          scheduleColorHex: "859039",
-          scheduleTitle: "코딩 공부",
-          scheduleDetail: "DateTime 공부",
-          scheduleDone: false,
-          scheduleMember: [],
-        ),
-        Schedule(
-          scheduleId: 2,
-          scheduleDate: DateTime(2023, 3, 10),
-          scheduleIndexList: [38, 39, 40, 41, 42, 43],
-          scheduleColorHex: '77DD77',
-          scheduleTitle: '코딩',
-          scheduleDetail: 'DB 구축 회의\n짱구 - 모델 설계\n철수 - API 설계\n맹구 - DB 및 서버 공급 서비스 선택\n맹구 - DB 및 서버 공급 서비스 선택\n맹구 - DB 및 서버 공급 서비스 선택\n맹구 - DB 및 서버 공급 서비스 선택 ',
-          scheduleDone: false,
-          scheduleMember: ['짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구', '철수', '맹구'],
-          scheduleLocation: '서울',
-        ),
-      ];
-    } else {
-      scheduleList = [
-        Schedule(
-          scheduleId: 3,
-          scheduleDate: DateTime(2023, 3, 15),
-          scheduleIndexList: [13, 14, 15, 16, 17],
-          scheduleColorHex: "859039",
-          scheduleTitle: "디자인 미팅",
-          scheduleDetail: "1. 홈 화면 UI 디자인 피드백\n2. 로고 디자인 요청",
-          scheduleDone: false,
-          scheduleMember: ['이히이'],
-        ),
-        Schedule(
-          scheduleId: 4,
-          scheduleDate: DateTime(2023, 3, 15),
-          scheduleIndexList: [41, 42, 43],
-          scheduleColorHex: '77DD77',
-          scheduleTitle: '치과 진료',
-          scheduleDone: false,
-          scheduleMember: ['짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구짱구', '철수', '맹구'],
-          scheduleLocation: '연세스마일치과',
-        ),
-      ];
+    final String date = DateFormat('yyyyMMdd').format(dateTime);
+    final Response response = await get('/schedule/$date');
+    if (response.hasError) {
+      errorHandler(response);
     }
-
+    scheduleList = (response.body['body']['list'] as List).map((e) => Schedule.fromJson(e)).toList();
     return scheduleList;
   }
 
   Future<void> createSchedule({required Schedule schedule}) async {
+    print('======> createSchedule');
+    await Future.delayed(const Duration(seconds: 2));
     // TODO : Connect API to Create Schedule
   }
 
   Future<void> updateSchedule({required Schedule schedule}) async {
+    print('======> updateSchedule');
+    await Future.delayed(const Duration(seconds: 2));
     // TODO : Connect API to Update Schedule
   }
 
   Future<void> deleteSchedule({required Schedule schedule}) async {
+    print('======> deleteSchedule');
+    await Future.delayed(const Duration(seconds: 2));
     // TODO : Connect API to Delete Schedule
   }
 }
