6c1e30bbaba1af67902eb0a9269bbb2eb62beac4
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-04-05 19:09:31 +0900

    [Fix] 강제 업데이트 무시 에러 해결

--- lib/view/init/controller/InitController.dart
+++ lib/view/init/controller/InitController.dart
@@ -43,59 +43,62 @@ class InitController extends RootController {
         if (isUpdateAlertBlocked) return;
       }
       return Get.dialog(
-        Padding(
-          padding: const EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Material(
-                borderRadius: BorderRadius.circular(16),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-                  child: Column(
-                    children: [
-                      Text(update.onMaintainance ? '점검 알림' : '업데이트 알림', style: StyledFont.HEADLINE),
-                      const SizedBox(height: 16),
-                      Text(update.reason, style: StyledFont.CALLOUT),
-                      const SizedBox(height: 16),
-                      if (!update.onMaintainance)
-                        GestureDetector(
-                          onTap: routeStoreToUpdate,
-                          child: Container(
-                              decoration: BoxDecoration(
-                                color: StyledPalette.STATUS_INFO,
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              width: double.infinity,
-                              padding: const EdgeInsets.symmetric(vertical: 8),
-                              alignment: Alignment.center,
-                              child: const Text('지금 업데이트', style: StyledFont.CALLOUT_WHITE)),
-                        )
-                    ],
+        WillPopScope(
+          onWillPop: () async => !update.isNecessary && !update.onMaintainance ? true : false,
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Material(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                    child: Column(
+                      children: [
+                        Text(update.onMaintainance ? '점검 알림' : '업데이트 알림', style: StyledFont.HEADLINE),
+                        const SizedBox(height: 16),
+                        Text(update.reason, style: StyledFont.CALLOUT),
+                        const SizedBox(height: 16),
+                        if (!update.onMaintainance)
+                          GestureDetector(
+                            onTap: routeStoreToUpdate,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  color: StyledPalette.STATUS_INFO,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                width: double.infinity,
+                                padding: const EdgeInsets.symmetric(vertical: 8),
+                                alignment: Alignment.center,
+                                child: const Text('지금 업데이트', style: StyledFont.CALLOUT_WHITE)),
+                          )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              if (!update.isNecessary && !update.onMaintainance)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () => blockUpdateAlert(updateId: update.id),
-                      child: const Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text('더 이상 안볼래요', style: StyledFont.CALLOUT_GRAY),
+                if (!update.isNecessary && !update.onMaintainance)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => blockUpdateAlert(updateId: update.id),
+                        child: const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text('더 이상 안볼래요', style: StyledFont.CALLOUT_GRAY),
+                        ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: Get.back,
-                      child: const Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text('다음에 할게요', style: StyledFont.CALLOUT_GRAY),
+                      GestureDetector(
+                        onTap: Get.back,
+                        child: const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text('다음에 할게요', style: StyledFont.CALLOUT_GRAY),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-            ],
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
         barrierDismissible: !update.isNecessary && !update.onMaintainance,
@@ -103,35 +106,38 @@ class InitController extends RootController {
     } catch (e) {
       print(e);
       return Get.dialog(
-        Padding(
-          padding: const EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Material(
-                borderRadius: BorderRadius.circular(16),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: const [
-                      Text('서비스 오류', style: StyledFont.HEADLINE, textAlign: TextAlign.center),
-                      SizedBox(height: 16),
-                      Text('현재 앱을 실행할 수 없다면\n아래 방법을 시도해주세요', style: StyledFont.CALLOUT),
-                      SizedBox(height: 16),
-                      Text('1. 네트워크 상태 확인', style: StyledFont.CALLOUT_700),
-                      Text('네트워크 상태가 고르지 않으면 저장된 일정을 불러오지 못해 서비스를 이용할 수 없어요', style: StyledFont.FOOTNOTE),
-                      SizedBox(height: 8),
-                      Text('2. 앱을 완전히 삭제 후 최신 버전 재설치', style: StyledFont.CALLOUT_700),
-                      Text('오류 발생 시 빠른 업데이트를 통해 사용자가 불편을 느끼지 않도록 최선을 다하고 있어요', style: StyledFont.FOOTNOTE),
-                      SizedBox(height: 8),
-                      Text('3. Tembird 팀에 문의', style: StyledFont.CALLOUT_700),
-                      Text('저희에게 겪고 계신 문제를 말씀해주시면 사용에 불편이 없도록 빠르게 처리해드릴게요', style: StyledFont.FOOTNOTE),
-                    ],
+        WillPopScope(
+          onWillPop: () async => false,
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Material(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: const [
+                        Text('서비스 오류', style: StyledFont.HEADLINE, textAlign: TextAlign.center),
+                        SizedBox(height: 16),
+                        Text('현재 앱을 실행할 수 없다면\n아래 방법을 시도해주세요', style: StyledFont.CALLOUT),
+                        SizedBox(height: 16),
+                        Text('1. 네트워크 상태 확인', style: StyledFont.CALLOUT_700),
+                        Text('네트워크 상태가 고르지 않으면 저장된 일정을 불러오지 못해 서비스를 이용할 수 없어요', style: StyledFont.FOOTNOTE),
+                        SizedBox(height: 8),
+                        Text('2. 앱을 완전히 삭제 후 최신 버전 재설치', style: StyledFont.CALLOUT_700),
+                        Text('오류 발생 시 빠른 업데이트를 통해 사용자가 불편을 느끼지 않도록 최선을 다하고 있어요', style: StyledFont.FOOTNOTE),
+                        SizedBox(height: 8),
+                        Text('3. Tembird 팀에 문의', style: StyledFont.CALLOUT_700),
+                        Text('저희에게 겪고 계신 문제를 말씀해주시면 사용에 불편이 없도록 빠르게 처리해드릴게요', style: StyledFont.FOOTNOTE),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         barrierDismissible: false,
